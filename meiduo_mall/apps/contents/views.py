74f140ba4bdccfea491e8a024e9c532849637202
zzZaida <17638121602@163.com>
2019-08-22 20:29:13 +0800

    5. 广告数据获取
    Signed-off-by: zzZaida <17638121602@163.com>

--- meiduo_mall/apps/contents/views.py
+++ meiduo_mall/apps/contents/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
 from django.views import View
 
+from apps.contents.models import ContentCategory
 from apps.contents.utils import get_categories
 
 
 class IndexView(View):
+    """首页广告"""
     def get(self, request):
 
-        # 1. 获取三级分类商品 数据
+        # 1.获取三级分类商品 数据
         categories = get_categories()
 
+        # 2.获取广告数据
+        contents = {}
+        # 2.1 获取所有的 广告分类
+        ad_categories = ContentCategory.objects.all()
+        # 2.2 遍历分类 取出每个分类广告
+        for category in ad_categories:
+            contents[category.key] = category.content_set.filter(status=True).order_by('sequence')
+
         context = {
-                    'categories': categories
+                    'categories': categories,
+                    'contents': contents,
                 }
 
         return render(request, 'index.html', context)
