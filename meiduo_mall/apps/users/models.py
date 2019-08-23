488a75edacc4cfd5997e7fac1a5ef30b52b10dc6
zzZaida <17638121602@163.com>
2019-08-23 17:59:50 +0800

    6.地址模型类 导入复制粘贴   User 新增字段 default_address   数据迁移
    Signed-off-by: zzZaida <17638121602@163.com>

--- meiduo_mall/apps/users/models.py
+++ meiduo_mall/apps/users/models.py
@@ -7,11 +7,15 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """自定义用户模型类"""
     # 系统自带 username password(加解密)
-    # 增加自己的字段 mobile 手机号
+    """新增 mobile 手机号 字段"""
     mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
 
     """新增 邮箱是否激活的 字段"""
     email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
+
+    """新增 默认地址 字段"""
+    default_address = models.ForeignKey('areas.Address', related_name='users', null=True, blank=True,
+                                        on_delete=models.SET_NULL, verbose_name='默认地址')
 
     class Meta:
         db_table = 'tb_users'
