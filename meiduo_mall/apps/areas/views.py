ced7f4b446d5e82977e47e8a96498a672e1f05f7
zzZaida <17638121602@163.com>
2019-08-23 16:43:04 +0800

    3. 获取省份的数据    1.SQL复习    2.ORM 操作省份数据 -->filter(parent__isnull=True) -->列表推导式--JSON格式 -->返回给前端
    Signed-off-by: zzZaida <17638121602@163.com>

--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -1,7 +1,44 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
+from apps.areas.models import Area
+from utils.response_code import RETCODE
+
 
 class AreasView(View):
+    """
+    SQL 省  select * from tb_areas where parent_id is NULL;
+        市  select * from tb_areas where parent_id = 130000;
+        县  select * from tb_areas where parent_id = 130100;
+
+    ORM 省  Area.objects.filter(parent__isnull=True)
+        市  Area.objects.filter(parent_id=area_id)
+        县  Area.objects.filter(parent_id=area_id )
+
+    """
     def get(self, request):
+
+        # 1.接收参数
+        area_id = request.GET.get('area_id')
+
+        if not area_id:
+            # 2.省的数据 area_id 为空
+            province_model_list = Area.objects.filter(parent__isnull=True)
+
+            # 将 ORM 的数据对象 转换成 给前端需要的 JSON 格式
+            province_list = []
+            for pro in province_model_list:
+                province_list.append({'id': pro.id,'name': pro.name})
+
+            return JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'province_list': province_list})
+        else:
+            # 3.市区的数据 area_id 有值
+            pass
+        # 4.将 ORM 的数据对象 转换成 给前端需要的 JSON 格式
+
+        # 5.返回前端数据
         return None
+
+
+
